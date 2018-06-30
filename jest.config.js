da64927fdc02d79e79ce7645b9dca4df7dd1c82a
Florian Mettetal <florian.mettetal@gmail.com>
2018-06-30 04:26:53 -0700

    :sparkles: Jest supports CSS and Files

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   "transform": {
     "^.+\\.tsx?$": "ts-jest"
   },
-  "setupTestFrameworkScriptFile": "<rootDir>/jest.setup.js",
+  "setupTestFrameworkScriptFile": "<rootDir>/jest/jest.setup.js",
   "testPathIgnorePatterns": ["/node_modules/", "/.reference/", "/build/", "/scripts/"],
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec)s?)\\.(jsx?|tsx?)$",
   "moduleFileExtensions": [
     "ts",
     "tsx",
@@ -19,5 +19,10 @@ module.exports = {
       "skipBabel": true,
       "tsConfigFile": "tsconfig.jest.json"
     }
-  }
+  },
+  "moduleNameMapper": {
+    "\\.(css|less)$": "identity-obj-proxy", // mock css
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/jest/mocks/fileMock.js",   // mock files
+  },
 }
